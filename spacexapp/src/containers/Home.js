3c6beef10ecd89df3917a4112f2d79fd11a8655a
Napatkrup <napatkrup@hotmail.com>
2021-02-18 21:57:15 +0700

    :rocket: Add Home COntainer + Fetch Info Data

--- spacexapp/src/containers/Home.js
+++ spacexapp/src/containers/Home.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 
 const Home = () => {
-    return (
-        <React.Fragment>
-            <Header />
-            <h1>Home Page</h1>
-        </React.Fragment>
-    );
+  const [info, setInfo] = useState([]);
+  useEffect(() => {
+    const fetchRockets = async () => {
+      const response = await fetch("https://api.spacexdata.com/v3/info");
+      const data = await response.json();
+      setInfo(data);
+    };
+    fetchRockets();
+  }, []);
+  return (
+    <React.Fragment>
+      <Header />
+      <h1>Home Page</h1>
+      <p>{JSON.stringify(info)}</p>
+    </React.Fragment>
+  );
 };
 
 export default Home;
